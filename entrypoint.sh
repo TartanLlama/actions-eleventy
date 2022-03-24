cb1566e3a1e96faf93677d3160d63333da413c14
Paul <64870518+woodcock3@users.noreply.github.com>
2022-03-24 16:10:52 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$INPUT_INSTALL_DEPENDENCIES" = "true" ]; then
   echo "Running \`npm install\`"
-  npm install && npm run $INPUT_BUILD_SCRIPT
+  $INPUT_BUILD_SCRIPT
 fi
 
 echo "Running eleventy"
